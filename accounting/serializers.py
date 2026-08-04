0664174cb350799899a53c453e89fb13070483f4
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2026-08-04 17:34:50 +0300

    added basic serializers for budget, category and attachment models

---
+++ accounting/serializers.py
@@ -0,0 +1,83 @@
+from django.db import transaction
+from rest_framework import serializers
+
+from .models import Category, Income, Expense, Budget, AccountingAttachment
+from organization.serializers import OrganizationReaderSerializer
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+        read_only_fields = ["id"]
+
+
+class CategoryShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ("id", "name",)
+
+
+class AccountingAttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountingAttachment
+        fields = ("id", "file", "created_at")
+        read_only_fields = ["created_at"]
+
+
+class BudgetWriterSerializer(serializers.ModelSerializer):
+    attachments = AccountingAttachmentSerializer(many=True, required=False)
+
+    class Meta:
+        model = Budget
+        fields = ("organization", "category", "planned_amount", "year", "month", "attachments")
+
+    def create(self, validated_data):
+        attachments_data = validated_data.pop("attachments", [])
+
+        with transaction.atomic():
+            budget = Budget.objects.create(**validated_data)
+
+            for attachment_data in attachments_data:
+                AccountingAttachment.objects.create(content_object=budget, **attachment_data)
+
+        return budget
+
+    def update(self, instance, validated_data):
+        attachments_data = validated_data.pop("attachments", [])
+
+        with transaction.atomic():
+            instance = super().update(instance, validated_data)
+
+            if attachments_data:
+                existing_attachments = {att.id: att for att in instance.attachments.all()}
+                keep_attachment_ids = []
+
+                for attachment_item in attachments_data:
+                    attachment_id = attachment_item.get("id", [])
+
+                    if attachment_id in existing_attachments:
+                        att_instance = existing_attachments[attachment_id]
+                        for key, value in attachment_item.items():
+                            setattr(att_instance, key, value)
+                        att_instance.save()
+                        keep_attachment_ids.append(att_instance.id)
+                    else:
+                        new_att = AccountingAttachment.objects.create(content_object=instance, **attachment_item)
+                        keep_attachment_ids.append(new_att.id)
+
+                for att_id, att_instance in existing_attachments.items():
+                    if att_id not in keep_attachment_ids:
+                        att_instance.delete()
+
+        return instance
+
+
+class BudgetReaderSerializer(serializers.ModelSerializer):
+    attachments = AccountingAttachmentSerializer(many=True, read_only=True)
+    organization = OrganizationReaderSerializer(read_only=True)
+    category = CategoryShortSerializer(read_only=True)
+
+    class Meta:
+        model = Budget
+        fields = "__all__"
